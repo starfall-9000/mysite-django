611a79806fe24f073b42aa3df12c3f312ae97365
anvu-trustcircle <an.vu@trustcircle.com>
2018-05-09 06:19:40 +0700

    add some DetailView testcase

--- polls/views.py
+++ polls/views.py
@@ -48,6 +48,13 @@ class DetailView(generic.DetailView):
   model = Question
   template_name = 'polls/detail.html'
 
+  def get_queryset(self):
+    """
+    Excludes any questions that aren't published yet.
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now())
+
+
 # def results(request, question_id):
 #   # response = "You're looking at the results of question %s."
 #   # return HttpResponse(response % question_id)
