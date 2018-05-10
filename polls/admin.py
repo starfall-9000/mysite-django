b13afc1e3f822fd2e47e28dfd5ddba086220e656
anvu-trustcircle <an.vu@trustcircle.com>
2018-05-11 05:35:44 +0700

    custom Question form by fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -6,7 +6,11 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-  fields = ['pub_date', 'question_text']
+  # fields = ['pub_date', 'question_text']
+  fieldsets = [
+    (None,               {'fields': ['question_text']}),
+    ('Date information', {'fields': ['pub_date']}),
+  ]
 
 admin.site.register(Question, QuestionAdmin)
 
