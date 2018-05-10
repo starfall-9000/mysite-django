70cfeae4fa87e786b4c201ead928d682e6f1471e
anvu-trustcircle <an.vu@trustcircle.com>
2018-05-11 05:46:32 +0700

    add StackedInline Choice in Question form

--- polls/admin.py
+++ polls/admin.py
@@ -5,14 +5,19 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInLine(admin.StackedInline):
+  model = Choice
+  extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
   # fields = ['pub_date', 'question_text']
   fieldsets = [
     (None,               {'fields': ['question_text']}),
     ('Date information', {'fields': ['pub_date']}),
   ]
+  inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
 
 # Register your models here.
